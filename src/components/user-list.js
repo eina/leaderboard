ffc4e40c247c0a13309f083a105128844a6d8b16
thelittleblacksmith <einers@outlook.com>
2017-03-30 17:57:20 -0700

    added profile link to static version

--- src/components/user-list.js
+++ src/components/user-list.js
@@ -3,12 +3,14 @@ import React from 'react';
 const UserList = (props) => {
   let users = props.users.map((user, i) => {
     let alt = user.username + '\'s profile photo';
+    let profile_url = 'https://www.freecodecamp.com/' + user.username;
+
     return (
       <tr key={i}>
         <td>{i + 1}</td>
         <td>
           <span><img src={user.img} alt={alt} className="profile-img"/></span>
-          <span>{user.username}</span>
+          <span><a href={profile_url}>{user.username}</a></span>
         </td>
         <td>{user.recent}</td>
         <td>{user.alltime}</td>
