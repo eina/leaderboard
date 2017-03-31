a88b52a4925593826f1e2a513331220208a1e2a4
thelittleblacksmith <einers@outlook.com>
2017-03-30 19:52:55 -0700

    finished toggle between recent and all time points

--- src/App.js
+++ src/App.js
@@ -6,37 +6,66 @@ import './App.css';
 //Components
 import UserList from './components/user-list'
 
-const URL = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
-
-// axios.get(URL)
-//   .then(function (response) {
-//     console.log(response.data);
-//   })
-
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = { user: [] };
+    this.state = { 
+      user: [],
+      category: 'https://fcctop100.herokuapp.com/api/fccusers/top/recent'
+    };
 
-    axios.get(URL)
+    this.show30Days = this.show30Days.bind(this);
+    this.showAllTime = this.showAllTime.bind(this);
+  }
+
+  componentDidMount(){
+    axios.get(this.state.category)
       .then((response) =>{
         this.setState({ user: response.data });
-      });
+      });   
+  }
+
+  show30Days(){
+    axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then((response) =>{
+        this.setState({ 
+          user: response.data,
+          category: 'https://fcctop100.herokuapp.com/api/fccusers/top/recent'   
+        });
+      });   
+  }
+
+  showAllTime(){
+    axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+      .then((response) =>{
+        this.setState({ 
+          user: response.data,
+          category: 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime'   
+        });
+      });   
   }
 
   render() {
     return (
       <div className="App">
         <Table responsive striped bordered hover>
-          <thead>
+          <caption>
+            <h1>freeCodeCamp Leaderboard</h1>
+            <p>
+              <button onClick={this.show30Days}>
+                Most Points in 30 Days</button>
+              <button onClick={this.showAllTime}>Most Points of All Time</button>
+            </p>            
+          </caption>
+          <thead>            
             <tr>
               <th>#</th>
               <th>Camper</th>
-              <th>Points in past 30 days <button className="btn">Sort</button></th>
-              <th>All time points <button className="btn">Sort</button></th>
+              <th>Points in past 30 days</th>
+              <th>All time points </th>
             </tr>            
-          </thead>
-          <caption><h1>freeCodeCamp Leaderboard</h1></caption>
+          </thead>          
+          {/*<UserList users={this.getData(this.state.category)}/>*/}
           <UserList users={this.state.user}/>
         </Table>
       </div>
